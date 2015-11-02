8b5cd8df649a34d49a57a5272e577b1b2d79c760
enzoProjects <enzuu@hotmail.com>
2015-11-02 01:29:58 -0300

    urlib1 intento de sacar frecuencia de letras y palabras y ver su distribucion, estuve chequeando y no estan para nada erradas con la realidad falta arreglar todo con un poco de 're' y listo va aquedar 100%

--- chapter_12/urlib1.py
+++ chapter_12/urlib1.py
@@ -1,18 +1,51 @@
 __author__ = 'springfield'
-
+from operator import itemgetter, attrgetter
 import urllib
+from BeautifulSoup import*
+
 #fhand = urllib.urlopen('http://www.py4inf.com/code/romeo.txt')
-larebelion = urllib.urlopen('http://www.samaelgnosis.net/libros/txt/gran_rebelion.txt')
+hunger = urllib.urlopen('https://archive.org/stream/Book1TheHungerGames/Book%201%20-%20The%20Hunger%20Games_djvu.txt')
 #for line in fhand:
 #    print line.strip()
 
-
 # al parecer como en c++ cuando uso algo de fhand (file handler) se borra de memoria
 
+#frecuencia de palabras
+"""
 frequencies = dict()
-for line in larebelion:
-    line.strip()
+for line in hunger:
+    unwanted_chars = "\".,-_!?</>/= "
     words=line.split()
-    for word in words:
+    for raw_word in words:
+        raw_word = raw_word.lower()
+        word = raw_word.strip(unwanted_chars)
         frequencies[word] = frequencies.get(word,0) +1
-print frequencies
+print sorted(frequencies, key=frequencies.__getitem__, reverse=True)
+"""
+
+#frecuencia de letras
+"""
+s=0
+
+frequencies = dict()
+for line in hunger:
+    unwanted_chars = "\"\'.,-_!?</>/\=[]{ }\xab\xc2 "
+    words=line.split()
+    for raw_word in words:
+        raw_word = raw_word.lower()
+        word = raw_word.strip(unwanted_chars)
+        word = list(word)
+        for letter in word:
+            frequencies[letter] = frequencies.get(letter,0) +1
+            s = s+1
+frec =[()]
+tot =0
+for i in frequencies.items():
+    tot = tot+float(i[1])/s*100
+    frec.append((float(i[1])/s*100,i[0]))
+
+sortedFrec = sorted(frec,reverse=True)
+for i in sortedFrec:
+    print 'letra: ' + str(i[1]) + ' aparece :' +str(i[0]) +' % '
+"""
+
